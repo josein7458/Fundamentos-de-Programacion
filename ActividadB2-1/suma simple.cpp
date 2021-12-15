aca39c002a70c92829db330d7768314931214866
Guevara Cunalata Josein Ernesto <92315586+josein7458@users.noreply.github.com>
2021-12-15 14:49:23 -0500

    Add files via upload

---
+++ ActividadB2-1/suma simple.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,z;
+	cout<<"ingrese su primer numero: "<<endl;
+	cin>>x;
+	cout<<"ingrese su segundo numero: "<<endl;
+	cin>>y;
+	z=x+y;
+	cout<<"el resultado de la suma es: "<<z<<endl;
+	return 0;
+}
